7fbef67adb6e01406a5323e0e504278fcc0d2f96
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-01 21:44:38 +0000

    data types

--- 01_basic/03_conversionoperration.js
+++ 01_basic/03_conversionoperration.js
@@ -21,6 +21,41 @@
 // // "histes" = true
 
 
-let somenum= 98
-let newnum=String(somenum)
-console.log(typeof newnum)
+// let somenum= 98
+// let newnum=String(somenum)
+// console.log(typeof newnum)
+// console.log("jiii");
+
+
+// let value = 4
+// let negvalue = - value
+// console.log(negvalue)
+
+
+// console.log(1+3+4);
+// console.log("1"+3+4); // in js if sting fisrt come then all vlue treat as string 
+
+// console.log(1+3+"4"); // in js if string is last so fisrt add value then print string as it as
+// console.log((2*3)+4);
+// console.log(+true);
+// console.log(+"");
+// let num1, num2, num3
+// num1=num2=num3= 2+2
+
+
+
+
+// let num = 100
+
+// console.log(++num);
+// console.log(num++);
+
+// // comaprisaom should be in same data type 
+// // avoid this blow comparison 
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined ==0);
+// console.log(undefined > 0);
+// console.log(undefined > 0);
