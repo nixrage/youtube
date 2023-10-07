7903f74f95b855fb3aeb7a339073c36147a32186
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-07 07:22:59 +0000

    mathandnum

---
+++ 01_basic/numandmath.js
@@ -0,0 +1,32 @@
+// const score= 400
+// console.log(score)
+// const balanc = new Number(100)
+// console.log(balanc)
+// console.log(typeof(balanc));
+
+// console.log(balanc.toString().length);//here i make balnce to string the i use string properties .length
+// console.log(balanc.toFixed(2));
+
+
+// const otherNum = 23.8565
+// console.log(otherNum.toPrecision(3));
+// //precision means precise value 
+
+// const hundreads = 10000000
+// console.log(hundreads.toLocaleString('en-IN'));// ye indian number sysem ke jaisa comma lagaiga 
+
+//==========================MATH========
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(5.3));
+console.log(Math.ceil(3,6,7));
+console.log(Math.floor(4,7));
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 24
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
