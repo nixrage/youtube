6bb5e91db9632ba6b4482c1dd0924f30ed697abe
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-08 08:18:52 +0000

    object

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -12,4 +12,20 @@
 // tinderuser.age=23
 // console.log(tinderuser);
 
+const user = {
+    email : "user1@gmail.com",
+    fullname:{
+        userfullname:{
+            firtsname:"pinky",
+            latname:"akp"
+            
+        }
+    }
+}
  
+// access nestes objected  using . 
+console.log(user.fullname.userfullname?.firtsname);
+
+//combin object 
+const obj1 = { 1:"a",2:"b",3:"c"}
+const obj2 = { 4:"er", 5:"sa",6:"sa"}
