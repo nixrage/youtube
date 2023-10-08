85d54119d360b4a0a0975fce87330d6dcc970da3
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-08 07:44:15 +0000

    04object

---
+++ 02_basic/04_object.js
@@ -0,0 +1,15 @@
+// declare object  two 
+
+// //non singleton object 
+// const tinderuser2 = {}
+// console.log(tinderuser2);
+
+// //singleton object 
+// const tinderuser = new Object()
+// console.log(tinderuser);
+// tinderuser.id="123user"
+// tinderuser.name="pname"
+// tinderuser.age=23
+// console.log(tinderuser);
+
+ 
