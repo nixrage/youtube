eacd366b69667fb3829b2c9a56a0baa4099468dc
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-10 06:49:16 +0000

    json

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,81 +1,100 @@
-// declare object  two 
+// // declare object  two 
 
-// //non singleton object 
-// const tinderuser2 = {}
-// console.log(tinderuser2);
+// // //non singleton object 
+// // const tinderuser2 = {}
+// // console.log(tinderuser2);
 
-// //singleton object 
-const tinderuser = new Object()
-console.log(tinderuser);
-tinderuser.id="123user"
-tinderuser.name="pname"
-tinderuser.age=23
+// // //singleton object 
+// const tinderuser = new Object()
 // console.log(tinderuser);
+// tinderuser.id="123user"
+// tinderuser.name="pname"
+// tinderuser.age=23
+// // console.log(tinderuser);
 
-const user = {
-    email : "user1@gmail.com",
-    fullname:{
-        userfullname:{
-            firtsname:"pinky",
-            latname:"akp"
+// const user = {
+//     email : "user1@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firtsname:"pinky",
+//             latname:"akp"
             
-        }
-    }
-}
+//         }
+//     }
+// }
  
-// access nestes objected  using . 
-console.log(user.fullname.userfullname?.firtsname);
+// // access nestes objected  using . 
+// console.log(user.fullname.userfullname?.firtsname);
 
-//combin object 
-const obj1 = { 1:"a",2:"b",3:"c"}
-const obj2 = { 4:"er", 5:"sa",6:"sa"}
+// //combin object 
+// const obj1 = { 1:"a",2:"b",3:"c"}
+// const obj2 = { 4:"er", 5:"sa",6:"sa"}
 
-// add onj1 and onj2 
-//const  obj3 = Object.assign({},obj1,obj2)// give this emapty {} because it comin ver well 
+// // add onj1 and onj2 
+// //const  obj3 = Object.assign({},obj1,obj2)// give this emapty {} because it comin ver well 
 
-//console.log(obj3);
+// //console.log(obj3);
 
-const obj4 = {...obj1, ...obj2} // spread method 
-//console.log(obj4);
-
-
-// values cmes from data base
-
-const newuser = [
-    {
-        id : 1,
-        email:"jiii@gmail.com"
-
-    },
-    {
-        id : 1,
-        email:"jiii@gmail.com"
-
-    },
-    {
-        id : 1,
-        email:"jiii@gmail.com"
-
-    },
-    {
-        id : 1,
-        email:"jiii@gmail.com"
-
-    },
-
-]
+// const obj4 = {...obj1, ...obj2} // spread method 
+// //console.log(obj4);
 
 
-console.log(newuser[2]);
-console.log(newuser[2].email);
+// // values cmes from data base
 
-console.log(tinderuser);
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));// array ke under array mila usko array kr liya 
+// const newuser = [
+//     {
+//         id : 1,
+//         email:"jiii@gmail.com"
 
-// check if vlaue exit or not (key h ya nahi ) properties availabe h ya nahi 
-console.log(tinderuser.hasOwnProperty('id'));
+//     },
+//     {
+//         id : 1,
+//         email:"jiii@gmail.com"
+
+//     },
+//     {
+//         id : 1,
+//         email:"jiii@gmail.com"
+
+//     },
+//     {
+//         id : 1,
+//         email:"jiii@gmail.com"
+
+//     },
+
+// ]
+
+
+// console.log(newuser[2]);
+// console.log(newuser[2].email);
+
+// console.log(tinderuser);
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));// array ke under array mila usko array kr liya 
+
+// // check if vlaue exit or not (key h ya nahi ) properties availabe h ya nahi 
+// console.log(tinderuser.hasOwnProperty('id'));
+
+//==========
+
+const course = {
+    cname:"js in hindi",
+    price : "999",
+    coursinstructor: "pink"
+}
+
+//console.log(course.cname);
+
+//use this syntax when we use key again again in code
+//destructure objects
+const {coursinstructor}= course
+console.log(coursinstructor);
+
+const {price}= course
+console.log(price);
+
 
 
 
