c7638b46d98ee59788da7e2ddaa4d4f28b532213
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-11 04:59:48 +0000

    arrow

--- 03_basic/02_basic.js
+++ 03_basic/02_basic.js
@@ -1,15 +1,44 @@
 
 
-function one(){
-    const username ="pinky"
+// function one(){
+//     const username ="pinky"
 
-    function two(){
-        const website = "utube"
-        console.log(username);
-    }
+//     function two(){
+//         const website = "utube"
+//         console.log(username);
+//     }
 
-    //console.log(website);
-    two()
+//     //console.log(website);
+//     two()
+// }
+// one() 
 
+//=============
+// if(true){
+//     const username = "pin"
+//     if (username=== "pin"){
+//         const website = "utube"
+//         console.log(username+ website);
+//     }
+//     //console.log(website);
+// }
+
+// //console.log(username);
+
+
+//============
+console.log(addme(7))
+function addme(num){
+    return num + 1
 }
-one() 
+
+
+
+
+
+//this is also function but sometimes we call its a expresion
+const addtwo = function (num){
+    return num +2
+}
+addtwo(3)
+ 
--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -3,48 +3,58 @@ const user={
     price : 877,
 
     welcomemessage: function(){
+        // use this when u want to acess variable ffrom current contest 
         console.log(`${this.usernamename} wlcome to website`);
         console.log(this);
     }
 
 
 
+
 }
 
-user.welcomemessage()
-user.usernamename="sam"
-user.welcomemessage()
+//this work in object only 
 
-//=========
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
+//user.welcomemessage()
+// user.usernamename="sam"
+// user.welcomemessage()
 
-// }
+// //=========
+// // function chai(){
+// //     let username = "hitesh"
+// //     con sole.log(this.username);
 
-// chai()
+// // }
 
-//create arrow function 
-// const  chai=()=>{
-//     let username = "hitesh"
-//     console.log(this);
+// // chai()
 
-// }
-// chai()
+// //create arrow function 
+// // const  chai=()=>{
+// //     let username = "hitesh"
+// //     console.log(this);
 
-
-// arrow function 
-// const addtwo=(num1, num2)=>{
-//     return num1+num2
-
-// }
-// console.log(addtwo(3,4));
-
-//const addtwo = (num1,num2)=>{return num1+num2}
-
-//return Object
-const addtwo = (num1,num2)=> ({ username:"pik"})
-
-console.log(addtwo(6,4));
+// // }
+// // chai()
 
 
+// // arrow function 
+//  () =>  {}
+// // const addtwo=(num1, num2)=>{
+// //     return num1+num2
+
+// // }
+// // console.log(addtwo(3,4));
+//{ } curly braces me wrap kiya toh teruen keyword likhna padega but jub () is me wrap kiya tab return likhne ki jararat nahi h 
+// //const addtwo = (num1,num2)=>{return num1+num2}
+// //const addtwo = (num1,num2)=>  num1+num2
+// //const addtwo = (num1,num2)=>  (num1+num2)
+// //return Object
+// const addtwo = (num1,num2)=> ({ username:"pik"})
+
+// console.log(addtwo(6,4));
+
+//===============
+// const myarry = [2,4,2,5,67,8]
+// myarry.forEach()
+
+start from 24
