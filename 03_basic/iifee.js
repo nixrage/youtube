4a5bc86993bfb3cd9ad602aa3c8536938f96cea2
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-17 06:50:04 +0000

    ifee

---
+++ 03_basic/iifee.js
@@ -0,0 +1,45 @@
+//imediatley invoked function expression(iifee)
+
+(function chai(){
+    //this names ifee q ki iska chain nam h 
+    console.log("hello chain");
+})();
+//()()
+//ifee
+
+//use ; at the end when u want to use more than 2 ifee funtion
+// (  () => {
+//     console.log(`db connected`);
+// })()
+
+//===
+(  (name) => {
+    console.log(`db connected ${name}`);
+})("hello im pinky ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
