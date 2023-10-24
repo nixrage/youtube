e840015d35396531ede4da21d2421c9f373461b4
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-24 10:36:16 +0000

    loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -10,16 +10,40 @@
 
 
 
-for (let i = 0; i <=10; i++) {
-    console.log(`outter loop ${i}`);
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outter loop ${i}`);
 
-    for (let j = 0; j < 10; j++) {
-        console.log(`inner oop ${j}`);
-        console.log(`${i}+${j}`);
-        console.log(i + "*" + j+ "=");
+//     for (let j = 1; j < 10; j++) {
+//         console.log(`inner oop ${j}`);
+//         console.log(i + "*" + j+ "=" + i*j);
          
         
-    }
+//     }
      
     
+// }
+
+
+// let myarray=["fllash","batman","superman","shaktiman"]
+// for (let index = 0; index < myarray.length; index++) {
+//     console.log( myarray.length);
+//     const element = myarray[index];
+//     console.log(element);
+    
+// }
+
+
+
+for (let i = 0; i <=20; i++) {
+     
+     if (i==5) {
+        console.log('seted 5');
+        //break
+        //break laga dega condition meet hone pe loop se bahar x
+        continue
+        //continue matlab maf krdo 5 ko 
+        
+     }
+
+     console.log(`value of i is ${i}`);
 }
