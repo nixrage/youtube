90b732648741f10e092540f0eda518fdcb02ff9e
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-24 11:43:09 +0000

    forin

---
+++ 05_iterations/four.js
@@ -0,0 +1,21 @@
+// const myobj={
+//     'a':"aplle",
+//     'b':"ball",
+//     'c':"cat"
+// }
+// //to intrates object use forin
+// for (const key in myobj) {
+//     // console.log(key);
+//     // console.log(myobj[key]);
+//     console.log(`keyand vle is key is ${key} and value is ${myobj[key]}`);
+     
+// }
+
+
+//use forin in for array '
+
+const myarra=['1','2','ds','s','sa']
+for (const key in myarra) {
+    console.log(myarra[key]);
+     
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+//["", "", ""]
+//[{},{},{}]
+
+// const arr= [1,2,3,4,5]
+// //use loop on onject,arry,dic
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greeting="happy BIRTHDAY"
+// for (const greet of greeting) {
+//     console.log(greet);
+    
+// }
+
+
+
+// const map= new Map()
+// map.set('91',"india")
+// map.set('32',"germany")
+// map.set('54',"france")
+
+//The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+//map unique value ke liye use karega and jo order diya gaya h usi order se rahega
+// console.log(map);
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+
+// }
+
+
+const myobj = {
+    'game1':'nfs',
+    'game2':'kio'
+}
+
+
+for (const obj of myobj) {
+    console.log(obj);
+    
+}
