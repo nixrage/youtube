b131fbf501c197948883b30da9442a7343f5ac8f
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-24 10:05:53 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,25 @@
+// //for loop
+// for(let index=0; index <= 10; index++){
+//     const element = index;
+//     console.log(element);
+//     if(element==5){
+//         console.log("5 is best number ");
+//     }
+// }
+
+
+
+
+for (let i = 0; i <=10; i++) {
+    console.log(`outter loop ${i}`);
+
+    for (let j = 0; j < 10; j++) {
+        console.log(`inner oop ${j}`);
+        console.log(`${i}+${j}`);
+        console.log(i + "*" + j+ "=");
+         
+        
+    }
+     
+    
+}
