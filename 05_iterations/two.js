feb72a27d65c23c493a417c89a128e66ff90c4d5
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-24 10:50:07 +0000

    dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// let index=6
+// while(index < 9){
+
+//     console.log("equal ");    index++;
+// }
+
+
+// let arr = 0;
+// let myarr = ["batmna","bhim","shaktiman"]
+// while (arr<myarr.length) {
+//     console.log(`value of array ${myarr[arr]}`);
+//     arr++;
+    
+// }
+ 
+
+let scrore = 1
+do{
+    console.log(`scrore is : ${scrore}`);
+    scrore++;
+
+}while (scrore < 10)  
