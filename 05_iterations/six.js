0c96640e21eb02e3b487aa95796c5005d9e1cacb
nixrage <124622109+nixrage@users.noreply.github.com>
2023-10-26 19:42:34 +0000

    six

---
+++ 05_iterations/six.js
@@ -0,0 +1,21 @@
+// const coding = ["js","python","java","ruby","bash","php"]
+
+// const values = coding.forEach(  (item)=>{
+//     //console.log(item);
+//     return item
+// }  )
+
+// console.log(values);
+
+//foreach values rerutn nahi karta 
+
+//filetr reyrtun value krta h 
+const mynum=[1,2,3,4,5,6,7,8,9,10]
+//fileter me condition dena padte ejo conditionn satisfy karega vo print hoga 
+// const newnum = mynum.filter( (num)=> {
+//     num>4 return newnum} )
+
+const newnum = mynum.filter( (num)=> num>4 )
+
+console.log(newnum)
+
